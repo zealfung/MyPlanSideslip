55a850e4031473d0558a90a7532e1e38003a2f03
Fengzy <362054694@qq.com>
2015-11-17 21:50:14 +0800

    将“影像”放入侧边栏，增加“任务”功能

---
+++ plan/UIViewControllers/Photo/PhotoViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotoViewController.h
+//  plan
+//
+//  Created by Fengzy on 15/11/17.
+//  Copyright © 2015年 Fengzy. All rights reserved.
+//
+
+#import "FatherTableViewController.h"
+
+@interface PhotoViewController : FatherTableViewController
+
+@end
---
+++ plan/UIViewControllers/Photo/PhotoViewController.m
@@ -0,0 +1,162 @@
+//
+//  PhotoViewController.m
+//  plan
+//
+//  Created by Fengzy on 15/11/17.
+//  Copyright © 2015年 Fengzy. All rights reserved.
+//
+
+#import "PhotoCell.h"
+#import "PhotoViewController.h"
+#import "AddPhotoViewController.h"
+#import "PhotoDetailViewController.h"
+
+//#import "PlanCache.h"
+//
+//#import "ThreeViewController.h"
+//
+//#import <RESideMenu/RESideMenu.h>
+
+
+@interface PhotoViewController () {
+    
+    NSArray *photoArray;
+}
+
+@end
+
+@implementation PhotoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = str_ViewTitle_5;
+    [self createNavBarButton];
+    
+    self.tableView.showsHorizontalScrollIndicator = NO;
+    self.tableView.showsVerticalScrollIndicator = NO;
+//    self.tableView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+    self.tableView.tableFooterView = [[UIView alloc] init];
+    
+    photoArray = [NSArray array];
+    [NotificationCenter addObserver:self selector:@selector(reloadPhotoData) name:Notify_Photo_Save object:nil];
+    [NotificationCenter addObserver:self selector:@selector(refreshTable) name:Notify_Photo_RefreshOnly object:nil];
+    
+    [self reloadPhotoData];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+- (void)dealloc {
+    
+    [NotificationCenter removeObserver:self];
+}
+
+- (void)createNavBarButton {
+    self.rightBarButtonItem = [self createBarButtonItemWithNormalImageName:png_Btn_Add selectedImageName:png_Btn_Add selector:@selector(addAction:)];
+}
+
+- (void)addAction:(UIButton *)button {
+    
+    AddPhotoViewController *controller = [[AddPhotoViewController alloc] init];
+    controller.operationType = Add;
+    controller.hidesBottomBarWhenPushed = YES;
+    [self.navigationController pushViewController:controller animated:YES];
+}
+
+- (void)reloadPhotoData {
+    
+    [self showHUD];
+    photoArray = [NSArray arrayWithArray:[PlanCache getPhoto]];
+    [self.tableView reloadData];
+    [self hideHUD];
+}
+
+- (void)refreshTable {
+    
+    [self.tableView reloadData];
+}
+
+#pragma mark - Table view data source
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    if (photoArray.count > 0) {
+        
+        return photoArray.count;
+        
+    } else {
+        
+        return 5;
+    }
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    if (photoArray.count > 0) {
+        
+        return kPhotoCellHeight;
+        
+    } else {
+        
+        return 44.f;
+    }
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    if (indexPath.row < photoArray.count) {
+        
+        tableView.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
+        Photo *photo = photoArray[indexPath.row];
+        PhotoCell *cell = [PhotoCell cellView:photo];
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+        
+        return cell;
+        
+    } else {
+        
+        static NSString *noticeCellIdentifier = @"noPhotoCellIdentifier";
+        
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:noticeCellIdentifier];
+        if (!cell) {
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:noticeCellIdentifier];
+            cell.backgroundColor = [UIColor clearColor];
+            cell.contentView.backgroundColor = [UIColor clearColor];
+            cell.selectionStyle = UITableViewCellSelectionStyleNone;
+            cell.textLabel.text = @"";
+            cell.textLabel.frame = cell.contentView.bounds;
+            cell.textLabel.textAlignment = NSTextAlignmentCenter;
+            cell.textLabel.textColor = [UIColor lightGrayColor];
+            cell.textLabel.font = font_Bold_16;
+        }
+        tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+        
+        if (indexPath.row == 4) {
+            cell.textLabel.text = str_Photo_Tips1;
+        } else {
+            cell.textLabel.text = nil;
+        }
+        
+        return cell;
+    }
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    if (indexPath.row < photoArray.count) {
+        
+        PhotoDetailViewController *controller = [[PhotoDetailViewController alloc] init];
+        controller.photo = photoArray[indexPath.row];
+        controller.hidesBottomBarWhenPushed = YES;
+        [self.navigationController pushViewController:controller animated:YES];
+    }
+}
+
+@end
