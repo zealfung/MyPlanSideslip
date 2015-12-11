88dce22f91b7a100abf052260fab918c78edbfc5
Fengzy <362054694@qq.com>
2015-12-11 18:42:14 +0800

    增加系统消息详情界面

---
+++ plan/UIViewControllers/More/Messages/MessagesDetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  MessagesDetailViewController.h
+//  plan
+//
+//  Created by Fengzy on 15/12/10.
+//  Copyright © 2015年 Fengzy. All rights reserved.
+//
+
+#import "Messages.h"
+#import "FatherViewController.h"
+
+@interface MessagesDetailViewController : FatherViewController
+
+@property (nonatomic, strong) Messages *message;
+
+@end
---
+++ plan/UIViewControllers/More/Messages/MessagesDetailViewController.m
@@ -0,0 +1,29 @@
+//
+//  MessagesDetailViewController.m
+//  plan
+//
+//  Created by Fengzy on 15/12/10.
+//  Copyright © 2015年 Fengzy. All rights reserved.
+//
+
+#import "MessagesDetailViewController.h"
+
+@interface MessagesDetailViewController () {
+    
+    UIScrollView *scrollView;
+}
+
+@end
+
+@implementation MessagesDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = str_ViewTitle_13;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
