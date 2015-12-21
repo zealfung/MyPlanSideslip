9971444b8b25d69b7d1119a351e07701adb594fa
Fengzy <362054694@qq.com>
2015-12-21 09:45:52 +0800

    增加“小区”自定义cell

---
+++ plan/UIViewControllers/Community/PostsCell.h
@@ -0,0 +1,13 @@
+//
+//  PostsCell.h
+//  plan
+//
+//  Created by Fengzy on 15/12/20.
+//  Copyright © 2015年 Fengzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostsCell : UITableViewCell
+
+@end
---
+++ plan/UIViewControllers/Community/PostsCell.m
@@ -0,0 +1,23 @@
+//
+//  PostsCell.m
+//  plan
+//
+//  Created by Fengzy on 15/12/20.
+//  Copyright © 2015年 Fengzy. All rights reserved.
+//
+
+#import "PostsCell.h"
+
+@implementation PostsCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
