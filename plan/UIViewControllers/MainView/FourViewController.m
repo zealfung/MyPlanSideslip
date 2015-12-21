989f0da40fa25e52124774afbcef1721720a635a
Fengzy <362054694@qq.com>
2015-12-21 09:46:12 +0800

    开发“小区”功能

--- plan/UIViewControllers/MainView/FourViewController.m
+++ plan/UIViewControllers/MainView/FourViewController.m
@@ -21,15 +21,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.title = @"小区";
     
     postsArray = [NSMutableArray array];
-    headerImagesURLArray = @[
-                                  @"https://ss2.baidu.com/-vo3dSag_xI4khGko9WTAnF6hhy/super/whfpf%3D425%2C260%2C50/sign=a4b3d7085dee3d6d2293d48b252b5910/0e2442a7d933c89524cd5cd4d51373f0830200ea.jpg",
-                                  @"https://ss0.baidu.com/-Po3dSag_xI4khGko9WTAnF6hhy/super/whfpf%3D425%2C260%2C50/sign=a41eb338dd33c895a62bcb3bb72e47c2/5fdf8db1cb134954a2192ccb524e9258d1094a1e.jpg",
-                                  @"http://c.hiphotos.baidu.com/image/w%3D400/sign=c2318ff84334970a4773112fa5c8d1c0/b7fd5266d0160924c1fae5ccd60735fae7cd340d.jpg"
+    headerImagesURLArray = @[@"https://ss2.baidu.com/-vo3dSag_xI4khGko9WTAnF6hhy/super/whfpf%3D425%2C260%2C50/sign=a4b3d7085dee3d6d2293d48b252b5910/0e2442a7d933c89524cd5cd4d51373f0830200ea.jpg",
+                             @"https://ss0.baidu.com/-Po3dSag_xI4khGko9WTAnF6hhy/super/whfpf%3D425%2C260%2C50/sign=a41eb338dd33c895a62bcb3bb72e47c2/5fdf8db1cb134954a2192ccb524e9258d1094a1e.jpg",
+                             @"http://c.hiphotos.baidu.com/image/w%3D400/sign=c2318ff84334970a4773112fa5c8d1c0/b7fd5266d0160924c1fae5ccd60735fae7cd340d.jpg",
+                                  @"http://file.bmob.cn/M02/FB/EF/oYYBAFZwp3uAbaT3AAYWayl5rTc443.png",
+                                  @"http://file.bmob.cn/M02/FB/EF/oYYBAFZwp3SAV2t-AASOgiiHmA4853.png",
+                                  @"http://file.bmob.cn/M02/FB/F0/oYYBAFZwp4KAfkI-AAg3Y3SaXls642.png"
                                   ];
 
-    headerTitlesArray = @[@"感谢您的支持，如果下载的",
+    headerTitlesArray = @[@"好‘计’友，一辈子",
+                          @"标题二",
+                          @"标题三",
+                          @"感谢您的支持，如果下载的",
                         @"如果代码在使用过程中出现问题",
                         @"您可以发邮件到gsdios@126.com",
                         @"感谢您的支持"
@@ -46,17 +52,18 @@
 }
 
 - (UIView *)createTableHeaderView {
-    CGFloat viewHeight = HEIGHT_FULL_VIEW / 3;
-    SDCycleScrollView *cycleScrollView2 = [SDCycleScrollView cycleScrollViewWithFrame:CGRectMake(0, 320, WIDTH_FULL_SCREEN, viewHeight) imageURLStringsGroup:nil]; // 模拟网络延时情景
+    CGFloat fullViewHeight = HEIGHT_FULL_VIEW;
+    CGFloat headerViewHeight = fullViewHeight / 3;
+    SDCycleScrollView *cycleScrollView2 = [SDCycleScrollView cycleScrollViewWithFrame:CGRectMake(0, 320, WIDTH_FULL_SCREEN, headerViewHeight) imageURLStringsGroup:headerImagesURLArray];
     cycleScrollView2.pageControlAliment = SDCycleScrollViewPageContolAlimentRight;
     cycleScrollView2.delegate = self;
     cycleScrollView2.titlesGroup = headerTitlesArray;
-    cycleScrollView2.dotColor = [UIColor yellowColor]; //自定义分页控件小圆标颜色
+    cycleScrollView2.dotColor = [UIColor whiteColor]; //自定义分页控件小圆标颜色
     cycleScrollView2.placeholderImage = [UIImage imageNamed:@"placeholder"];
     //--- 模拟加载延迟
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        cycleScrollView2.imageURLStringsGroup = headerImagesURLArray;
-    });
+//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+//        cycleScrollView2.imageURLStringsGroup = headerImagesURLArray;
+//    });
     return cycleScrollView2;
 }
 
