f20aeccb9951b8b10411aaf04147e4335024a3d7
Fengzy <362054694@qq.com>
2016-01-01 11:17:46 +0800

    修复跨月跨年签到不连续问题

--- plan/Utils/StatisticsCenter/StatisticsCenter.m
+++ plan/Utils/StatisticsCenter/StatisticsCenter.m
@@ -172,13 +172,13 @@
 }
 
 + (BOOL)isContinuousDate:(NSDate*)date1 date2:(NSDate*)date2 {
-    NSCalendar *calendar = [NSCalendar currentCalendar];
-    unsigned unitFlags = NSYearCalendarUnit | NSMonthCalendarUnit |  NSDayCalendarUnit;
-    NSDateComponents *comp1 = [calendar components:unitFlags fromDate:date1];
-    NSDateComponents *comp2 = [calendar components:unitFlags fromDate:date2];
-    return [comp2 day] - [comp1 day] == 1 &&
-    [comp1 month] == [comp2 month] &&
-    [comp1 year] == [comp2 year];
+    NSTimeInterval time = [date2 timeIntervalSinceDate:date1];
+    int days = ((int)time)/(3600*24);
+    if (days < 1) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 @end
