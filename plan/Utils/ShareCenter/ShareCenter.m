7512d8f08ab60fc46ea2c63239da4cebdedf93a2
Fengzy <362054694@qq.com>
2016-01-01 17:49:42 +0800

    分享类型改为自动

--- plan/Utils/ShareCenter/ShareCenter.m
+++ plan/Utils/ShareCenter/ShareCenter.m
@@ -29,7 +29,7 @@
                                      images:imageArray
                                         url:[NSURL URLWithString:str_Website_URL]
                                       title:str_App_Slogan
-                                       type:SSDKContentTypeWebPage];
+                                       type:SSDKContentTypeAuto];
     
     //1.2、自定义分享平台
     NSMutableArray *activePlatforms = [NSMutableArray arrayWithArray:@[@(SSDKPlatformTypeSinaWeibo), @(SSDKPlatformSubTypeWechatSession), @(SSDKPlatformSubTypeWechatTimeline), @(SSDKPlatformSubTypeQQFriend), @(SSDKPlatformSubTypeQZone), @(SSDKPlatformTypeQQ)]];
@@ -98,7 +98,7 @@
                                      images:imageArray
                                         url:[NSURL URLWithString:shareUrl]
                                       title:title
-                                       type:SSDKContentTypeWebPage];
+                                       type:SSDKContentTypeAuto];
     
     //1.2、自定义分享平台
     NSMutableArray *activePlatforms = [NSMutableArray arrayWithArray:@[@(SSDKPlatformTypeSinaWeibo), @(SSDKPlatformSubTypeWechatSession), @(SSDKPlatformSubTypeWechatTimeline), @(SSDKPlatformSubTypeQQFriend), @(SSDKPlatformSubTypeQZone), @(SSDKPlatformTypeQQ)]];
