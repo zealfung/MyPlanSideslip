f5be6d3f68a2db9509987ab078cb990f18bf15bd
Fengzy <362054694@qq.com>
2016-04-15 22:21:25 +0800

    增加“个人设置”界面

---
+++ plan/UIViewControllers/More/Settings/SettingsPersonalViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsPersonalViewController.h
+//  plan
+//
+//  Created by Fengzy on 16/4/15.
+//  Copyright © 2016年 Fengzy. All rights reserved.
+//
+
+#import "FatherViewController.h"
+
+@interface SettingsPersonalViewController : FatherViewController
+
+@end
