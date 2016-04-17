bff20b8ef2aa31b8f82f4373142ab2111428f1b3
Fengzy <362054694@qq.com>
2016-04-17 11:42:09 +0800

    增加通知消息公共变量类文件

---
+++ plan/Utils/Consts/NotificationConst.h
@@ -0,0 +1,13 @@
+//
+//  NotificationConst.h
+//  plan
+//
+//  Created by Fengzy on 16/4/16.
+//  Copyright © 2016年 Fengzy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NotificationConst : NSObject
+
+@end
---
+++ plan/Utils/Consts/NotificationConst.m
@@ -0,0 +1,13 @@
+//
+//  NotificationConst.m
+//  plan
+//
+//  Created by Fengzy on 16/4/16.
+//  Copyright © 2016年 Fengzy. All rights reserved.
+//
+
+#import "NotificationConst.h"
+
+@implementation NotificationConst
+
+@end
