788e976983da1604a38170feef33a6bf0677b19d
joey <362054694@qq.com>
2016-12-21 11:03:48 +0800

    补充缺少的两个文件，方便源码爱好者通过编译

---
+++ plan/Utils/Consts/IDStringConst.h
@@ -0,0 +1,26 @@
+//
+//  IDStringConst.h
+//  plan
+//
+//  Created by Fengzy on 16/4/27.
+//  Copyright © 2016年 Fengzy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/** Bmob后台应用ID */
+UIKIT_EXTERN NSString *const IDSTRBmobApplicationID;
+/** 新浪微博AppKey */
+UIKIT_EXTERN NSString *const IDSTRSinaWeiboAppKey;
+/** 新浪微博AppSecret */
+UIKIT_EXTERN NSString *const IDSTRSinaWeiboAppSecret;
+/** QQAppID */
+UIKIT_EXTERN NSString *const IDSTRQQAppID;
+/** QQAppKey */
+UIKIT_EXTERN NSString *const IDSTRQQAppKey;
+/** WechatAppKey */
+UIKIT_EXTERN NSString *const IDSTRWechatAppKey;
+/** WechatAppSecret */
+UIKIT_EXTERN NSString *const IDSTRWechatAppSecret;
+/** ShareSDKAppKey */
+UIKIT_EXTERN NSString *const IDSTRShareSDKAppKey;
---
+++ plan/Utils/Consts/IDStringConst.m
@@ -0,0 +1,26 @@
+//
+//  IDStringConst.m
+//  plan
+//
+//  Created by Fengzy on 16/4/27.
+//  Copyright © 2016年 Fengzy. All rights reserved.
+//
+
+#import "IDStringConst.h"
+
+/** Bmob后台应用ID */
+NSString *const IDSTRBmobApplicationID = @"";
+/** 新浪微博AppKey */
+NSString *const IDSTRSinaWeiboAppKey = @"";
+/** 新浪微博AppSecret */
+NSString *const IDSTRSinaWeiboAppSecret = @"";
+/** QQAppID */
+NSString *const IDSTRQQAppID = @"";
+/** QQAppKey */
+NSString *const IDSTRQQAppKey = @"";
+/** WechatAppKey */
+NSString *const IDSTRWechatAppKey = @"";
+/** WechatAppSecret */
+NSString *const IDSTRWechatAppSecret = @"";
+/** ShareSDKAppKey */
+NSString *const IDSTRShareSDKAppKey = @"";
